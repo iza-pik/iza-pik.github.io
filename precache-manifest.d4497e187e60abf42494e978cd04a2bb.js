907d92129ceb7db80117c17c44e1c0bbefbd8b47
Izabela <i.j.pikula@gmail.com>
2019-04-06 17:43:50 +0100

    Updates

---
+++ precache-manifest.d4497e187e60abf42494e978cd04a2bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b6f28745edb2cf68928",
+    "url": "/static/css/main.1019cbe3.chunk.css"
+  },
+  {
+    "revision": "9b6f28745edb2cf68928",
+    "url": "/static/js/main.109145e0.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2cfb18796040b943221c",
+    "url": "/static/css/2.2c59b2c6.chunk.css"
+  },
+  {
+    "revision": "2cfb18796040b943221c",
+    "url": "/static/js/2.6b438397.chunk.js"
+  },
+  {
+    "revision": "1715623a1278b38f6f6b69271186d7bf",
+    "url": "/index.html"
+  }
+];
