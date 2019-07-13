ad020db4c0966ca402dc1c0b119f0b0cb5d81cf3
Izabela <i.j.pikula@gmail.com>
2019-07-13 15:55:58 +0100

    added my CV and a link to download it

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
 import Button from "../../components/button";
 import "./home.css";
+import { Link } from "react-router-dom";
+import myCV from "../../assets/docs/Izabela Pikula - CV - 13-07-2019.pdf";
 
 class Home extends Component {
   render() {
     return (
       <main className="App-main">
+        <p>React developer</p>
         <p>Translator</p>
         <p>Proofreader</p>
         <p>Project Manager</p>
-        <p>Aspiring software developer</p>
         <p>Hobbyist photographer and painter</p>
         <img
           src="https://bit.ly/2UPFGCB"
           className="App-icon"
           alt="Curly brackets icon"
         />
-        <Button className="btn-style">Download my CV</Button>
+        <Link to={myCV} target="_blank" download>
+          <Button className="btn-style">Download my CV</Button>
+        </Link>
       </main>
     );
   }
