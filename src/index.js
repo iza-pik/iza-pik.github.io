f80af9517b0257eebb443b50ebea359c58c811ea
Izabela <i.j.pikula@gmail.com>
2019-07-20 15:19:55 +0100

    added lazy loading function

--- src/index.js
+++ src/index.js
@@ -1,49 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Home from "./pages/home/";
-// import About from "./pages/about/";
-// import Contacts from "./pages/contacts/";
-// import Projects from "./pages/projects/";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const Home = React.lazy(() =>
-  import(/* webpackChunkName: "Home", webpackPrefetch: true */ "./pages/home")
-);
+  import(/* webpackChunkName: "Home", webpackPrefetch: true */ './pages/home'),
+)
 const About = React.lazy(() =>
-  import(/* webpackChunkName: "About", webpackPrefetch: true */ "./pages/about")
-);
+  import(
+    /* webpackChunkName: "About", webpackPrefetch: true */ './pages/about'
+  ),
+)
 const Contacts = React.lazy(() =>
   import(
-    /* webpackChunkName: "Contacts", webpackPrefetch: true */ "./pages/contacts"
-  )
-);
+    /* webpackChunkName: "Contacts", webpackPrefetch: true */ './pages/contacts'
+  ),
+)
 const Projects = React.lazy(() =>
   import(
-    /* webpackChunkName: "Projects", webpackPrefetch: true */ "./pages/projects"
-  )
-);
+    /* webpackChunkName: "Projects", webpackPrefetch: true */ './pages/projects'
+  ),
+)
 
 ReactDOM.render(
   <Router>
     <App>
       <React.Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contacts" component={Contacts} />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/contacts' component={Contacts} />
         </Switch>
       </React.Suspense>
     </App>
   </Router>,
-  document.getElementById("root")
-);
+  document.getElementById('root'),
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
